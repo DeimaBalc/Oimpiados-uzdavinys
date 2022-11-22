bf1913b50ccb47251176cad5bf4e2ac5934f1474
Deima <deima.balciute@gmail.com>
2022-11-22 20:03:24 +0200

    Finally done

--- sprendimas.cpp
+++ sprendimas.cpp
@@ -3,53 +3,73 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
-    ifstream fin ("duom.txt");
-    ofstream fout ("rez.txt");
+    ifstream fin("duom.txt");
+    ofstream fout("rez.txt");
 
-    int gatve, auk[100], rez[100];
+    int gatve, auk[100], rez[100], max;
     bool grozis;
 
     fin >> gatve;
 
-    for (int i = 0; i<gatve; i++){
+    for (int i = 0; i < gatve; i++)
+    {
 
         fin >> auk[i];
-        
-
     }
-    
-    for (int j = 0; j<gatve; j++ ) {
 
-        for (int n=j+1; n<gatve; n++){
+    for (int j = 0; j < gatve; j++)
+    {
 
-            if (auk[j] > auk[n]){
+        max = 0;
+
+        for (int n = j + 1; n < gatve; n++)
+        {
+
+            if (auk[j] > auk[n])
+            {
 
                 grozis = true;
-
-            } else {
+            }
+            else
+            {
 
                 grozis = false;
-
             }
 
-            if (grozis == true){
+            if (grozis == true)
+            {
 
-                rez[j] = 0;
+                continue;
+            }
+            else if (max < auk[n])
+            {
 
-            } else {
+                max = auk[n];
+            }
+            else
+            {
 
-                rez[j] = auk[n] - auk[j] + 1;
-
+                continue;
             }
         }
 
+        if (max == 0)
+        {
+
+            rez[j] = 0;
+        }
+        else
+        {
+
+            rez[j] = max - auk[j] + 1;
+        }
+
         fout << rez[j] << ' ';
-        cout << rez[j];
+        cout << rez[j] << endl;
     }
-
-
 
     fin.close();
     fout.close();
